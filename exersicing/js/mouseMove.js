f74063877c4d9834873ab09f2d6719bf7259b940
서지상 <inaki96@naver.com>
2021-04-25 20:16:36 +0900

    requestFrame added

--- exersicing/js/mouseMove.js
+++ exersicing/js/mouseMove.js
@@ -5,15 +5,53 @@ window.onload = () =>{
     let cursor_div = document.getElementsByClassName('cursor_item')[0];
 
 
+    const mouse = {
+        x: 0,
+        y: 100,
+        mx: 0,
+        my: 0,
+        speed: 0.03
+    };
 
-    console.dir(cursor_div);
+    const lineSizeV = cursor_div.offsetWidth;
+    const lineSizeH = cursor_div.offsetHeight;
+
+   
+
     window.addEventListener("mousemove",(e)=>{
 
-        h1.innerHTML = `x:${e.clientX} y:${e.clientY}`;
+        mouse.x = e.clientX;
+        mouse.y = e.clientY;
 
-        cursor_div.style.transform = 'translate('+ e.clientX + 'px,' +  e.clientY + 'px)';
+        
+  
+    
     })
 
-}
+    
+    const loop =() =>{
 
-//화면이 다 로드 된 후에 실행하라
+        
+      
+
+        mouse.mx += (mouse.x-lineSizeH/2-mouse.mx)*mouse.speed;
+        mouse.my += (mouse.y-lineSizeV/2-mouse.my)*mouse.speed;
+
+        // 조금씩 작은 수를 더해주어 볼록 곡선 속도 그래프 형태
+
+        h1.innerHTML = `x:${mouse.mx} y:${mouse.my}`;
+        cursor_div.style.transform = 'translate(' + mouse.mx + 'px,' + mouse.my + 'px)';
+
+
+
+        window.requestAnimationFrame(loop);
+        
+
+    }
+
+    loop();
+
+
+
+
+}
